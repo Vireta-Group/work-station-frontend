705de876d859f7993f59ebdeb359d93a61cd7eee
omur faruk <omurfaruk39bd@gmail.com>
2025-08-20 22:27:48 +0600

    add sidebar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router";
 import HeaderNav from "../components/headerNavbar/headerNav";
+import Sidebar from "../components/sidebar/Sidebar";
 
 function Root() {
   return (
     <div>
-     <HeaderNav/>
-    
+      <HeaderNav />
+      <Sidebar />
       <Outlet />
     </div>
   );
