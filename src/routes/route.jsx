061a88b5c4f51cd28aa44b4ccaef5ee9c421f954
mdnahid47 <nmd97243@gmail.com>
2025-08-23 00:30:55 +0600

    taskSubmit

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -5,6 +5,7 @@ import {
 } from "react-router";
 import Root from "./root";
 import HeaderNav from "../components/headerNavbar/headerNav";
+import TaskSubmit from "../coreModule/userPanel/taskSubmit/TaskSubmit";
 
 
 const route = createBrowserRouter(
@@ -12,6 +13,7 @@ const route = createBrowserRouter(
     <Route path="/" element={<Root />}>
       <Route path="about" element={<p className="text-red-500"> hell </p>} />
       <Route path="headerNav" element={<HeaderNav/>}></Route>
+      <Route path="taskSubmit" element={<TaskSubmit />} />
     </Route>
   )
 );
