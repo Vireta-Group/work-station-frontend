bb924972e13117404c83224ccc8033645fb4e025
omur faruk <omurfaruk39bd@gmail.com>
2025-08-22 17:58:09 +0600

    adding table in WorkDis.jsx

--- src/components/headerNavbar/HeaderNav.jsx
+++ src/components/headerNavbar/HeaderNav.jsx
@@ -1,24 +1,24 @@
-import logoheader from "../../assets/logo/logo.png"
-import user from "../../assets/logo/user.png"
+import logoheader from "../../assets/logo/logo.png";
+import user from "../../assets/logo/user.png";
 const HeaderNav = () => {
   return (
-    <nav className="flex overflow-clip px-[2%] py-1 h-[70px] w-full justify-between items-center bg-[#1C352D] shadow-md ">
+    <nav className="flex overflow-clip px-[2%] py-1 h-[70px] w-full justify-between items-center bg-[#1C352D] shadow-md fixed z-10">
+      <div className="logo">
+        <img className="w-[4rem]" src={logoheader} alt="logoHeader" />
+      </div>
 
-        <div className="logo">
-            <img className="w-[4rem]" src={logoheader} alt="logoHeader" />
+      <div className="user ">
+        <div className="flex flex-col justify-center items-center ">
+          <img
+            className="w-[2.2rem] border-2 rounded-full "
+            src={user}
+            alt=""
+          />
+          <h2 className="text-shadow-lg font-semibold ">John David</h2>
         </div>
-
-        <div className="user ">
-          
-          <div className="flex flex-col justify-center items-center ">
-          <img className="w-[2.2rem] border-2 rounded-full " src={user} alt="" />
-           <h2 className="text-shadow-lg font-semibold ">John David</h2>
-          </div>
-        </div>
-
-
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default HeaderNav
+export default HeaderNav;
--- src/coreModule/teamAdminPanel/workDistrubition/WorkDis.jsx
+++ src/coreModule/teamAdminPanel/workDistrubition/WorkDis.jsx
@@ -7,6 +7,7 @@ import {
   Select,
   MenuItem,
 } from "@mui/material";
+import JobTable from "./JobTable";
 
 export default function WorkDistributionForm({ onSubmit }) {
   const [formData, setFormData] = useState({
@@ -56,94 +57,102 @@ export default function WorkDistributionForm({ onSubmit }) {
   };
 
   return (
-    <div className="p-6 bg-gray-800 rounded-lg h-[100%] max-w-[700px] mx-auto mt-10">
-      <h2 className="text-2xl font-bold text-white mb-4 text-center">
-        Work Distribution Form
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <FormControl
-          label="Today's Date"
-          type="date"
-          value={today}
-          InputLabelProps={{ shrink: true }}
-          InputProps={{ readOnly: true }}
-          sx={{ m: 2, width: "45%" }}
-        >
-          <TextField value={today} sx={muiInputStyles} />
-        </FormControl>
-
-        <FormControl sx={{ m: 2, width: "45%" }}>
-          <TextField
-            label="Job Title"
-            name="jobTitle"
-            value={formData.jobTitle}
-            onChange={handleChange}
-            required
-            sx={muiInputStyles}
-          />
-        </FormControl>
-
-        <FormControl fullWidth className="mb-4" sx={{ my: 2 }}>
-          <TextField
-            label="Job Description"
-            name="jobDescription"
-            value={formData.jobDescription}
-            onChange={handleChange}
-            multiline
-            rows={4}
-            required
-            fullWidth
-            sx={muiInputStyles}
-          />
-        </FormControl>
-
-        <FormControl fullWidth className="mb-4" sx={{ my: 2 }}>
-          <TextField
-            label="Expiration Date"
-            name="expirationDate"
+    <div className="flex flex-col justify-center w-[100%]">
+      <div className="p-6 bg-gray-800 rounded-lg h-[100%] max-w-[700px] mx-auto mt-10">
+        <h2 className="text-2xl font-bold text-white mb-4 text-center">
+          Work Distribution Form
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <FormControl
+            label="Today's Date"
             type="date"
-            value={formData.expirationDate}
-            onChange={handleChange}
+            value={today}
             InputLabelProps={{ shrink: true }}
-            required
-            fullWidth
-            sx={muiInputStyles}
-          />
-        </FormControl>
+            InputProps={{ readOnly: true }}
+            sx={{ m: 2, width: "45%" }}
+          >
+            <TextField value={today} sx={muiInputStyles} />
+          </FormControl>
 
-        <FormControl fullWidth sx={{ my: 2 }}>
-          <InputLabel sx={{ color: "white" }}>Assign User</InputLabel>
-          <Select
-            label="Assign User"
-            name="assignedUser"
-            value={formData.assignedUser}
-            onChange={handleChange}
-            required
-            sx={{
-              ...muiInputStyles,
-              "& .MuiSelect-icon": {
-                color: "white",
-              },
-            }}
-          >
-            {users.map((user) => (
-              <MenuItem key={user.id} value={user.id} sx={{ color: "white" }}>
-                {user.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <div className="flex justify-center">
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            sx={{ width: "200px", my: 4, alignSelf: "center", height: "50px" }}
-          >
-            Submit
-          </Button>
-        </div>
-      </form>
+          <FormControl sx={{ m: 2, width: "45%" }}>
+            <TextField
+              label="Job Title"
+              name="jobTitle"
+              value={formData.jobTitle}
+              onChange={handleChange}
+              required
+              sx={muiInputStyles}
+            />
+          </FormControl>
+
+          <FormControl fullWidth className="mb-4" sx={{ my: 2 }}>
+            <TextField
+              label="Job Description"
+              name="jobDescription"
+              value={formData.jobDescription}
+              onChange={handleChange}
+              multiline
+              rows={4}
+              required
+              fullWidth
+              sx={muiInputStyles}
+            />
+          </FormControl>
+
+          <FormControl fullWidth className="mb-4" sx={{ my: 2 }}>
+            <TextField
+              label="Expiration Date"
+              name="expirationDate"
+              type="date"
+              value={formData.expirationDate}
+              onChange={handleChange}
+              InputLabelProps={{ shrink: true }}
+              required
+              fullWidth
+              sx={muiInputStyles}
+            />
+          </FormControl>
+
+          <FormControl fullWidth sx={{ my: 2 }}>
+            <InputLabel sx={{ color: "white" }}>Assign User</InputLabel>
+            <Select
+              label="Assign User"
+              name="assignedUser"
+              value={formData.assignedUser}
+              onChange={handleChange}
+              required
+              sx={{
+                ...muiInputStyles,
+                "& .MuiSelect-icon": {
+                  color: "white",
+                },
+              }}
+            >
+              {users.map((user) => (
+                <MenuItem key={user.id} value={user.id} sx={{ color: "white" }}>
+                  {user.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <div className="flex justify-center">
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{
+                width: "200px",
+                my: 4,
+                alignSelf: "center",
+                height: "50px",
+              }}
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
+      <JobTable />
     </div>
   );
 }
