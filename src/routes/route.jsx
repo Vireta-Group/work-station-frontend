4ed0ada2b549ad2b2fa9af1df71df6732b76eb65
momin109 <momin550550@gmail.com>
2025-08-27 08:21:43 +0600

    updatte employee

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -4,12 +4,11 @@ import {
   Route,
 } from "react-router";
 import Root from "./root";
+import AddEmployee from "../coreModule/hrAdmin/addEmployee/AddEmployee";
 // import LoginPage from "../components/loginPage/LoginPage.jsx";
 // import ProtectRoute from "./ProtectRoute.jsx";
 // import WorkDis from "../coreModule/teamAdminPanel/workDistrubition/WorkDis.jsx";
 // import EWorkDetail from "../coreModule/teamAdminPanel/EWorkDetail/EWorkDetail.jsx";
-
-
 
 const route = createBrowserRouter(
   createRoutesFromElements(
@@ -18,6 +17,7 @@ const route = createBrowserRouter(
       {/* <Route path="/" element={<ProtectRoute />}> */}
       <Route path="" element={<Root />}>
         <Route path="" element={<p className="text-text-muted"> hell </p>} />
+        <Route path="employee-form" element={<AddEmployee />} />
         {/* <Route path="work-dis" element={<WorkDis />} />
           <Route path="e-work" element={<EWorkDetail />} /> */}
       </Route>
