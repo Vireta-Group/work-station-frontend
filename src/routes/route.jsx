c9dd6d3fc441de6d56b79a3b24ea491557b82904
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2025-08-28 21:18:42 +0600

    Work submission page added

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -5,6 +5,7 @@ import {
 } from "react-router";
 import Root from "./root";
 import SignIn from "../pages/AuthPages/SignIn";
+import WorkSubmission from "../coreModule/userPanel/workSubmission/WorkSubmission";
 // import LoginPage from "../components/loginPage/LoginPage.jsx";
 // import ProtectRoute from "./ProtectRoute.jsx";
 // import WorkDis from "../coreModule/teamAdminPanel/workDistrubition/WorkDis.jsx";
@@ -19,7 +20,8 @@ const route = createBrowserRouter(
       {/* <Route path="/" element={<ProtectRoute />}> */}
       <Route path="" element={<Root />}>
         <Route path="" element={<p className="text-text-muted"> hell </p>} />
-        <Route path="signIn" element={<SignIn></SignIn>} />
+        <Route path="signIn" element={<SignIn />} />
+        <Route path="workSubmission" element={<WorkSubmission />} />
         {/* <Route path="work-dis" element={<WorkDis />} />
           <Route path="e-work" element={<EWorkDetail />} /> */}
       </Route>
