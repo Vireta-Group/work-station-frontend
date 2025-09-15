5de19a4c630310e1032542168a9bccd3fab30a74
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2025-09-15 21:34:50 +0600

    add department & team leader added

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -23,6 +23,8 @@ import EditEmployee from "../coreModule/hrAdmin/editEmployee/EditEmployee";
 import EditEmployeeDashboard from "../coreModule/hrAdmin/editEmployee/EditEmployeeDashboard";
 import SupAdDashbord from "../coreModule/superAdmin/dashbord/SupAdDashbord";
 import EmployeeList from "../coreModule/superAdmin/employeeList/EmployeeList";
+import AddDepartment from "../coreModule/superAdmin/addDepartment/AddDepartment";
+import AddTeamLeader from "../coreModule/superAdmin/addDepartment/AddTeamLeader";
 
 const route = createBrowserRouter(
   createRoutesFromElements(
@@ -57,8 +59,10 @@ const route = createBrowserRouter(
         {/* mahbub.................. */}
         <Route path="employee-form" element={<AddEmployee />} />
         <Route path="teamadminpanel" element={<TeamAdminPanel />} />
-        <Route path="SupAdDashbord" element={<SupAdDashbord/>}/>         {/* mahbub.................. */}
-        <Route path="employeeList" element={<EmployeeList/>}/>
+        <Route path="SupAdDashbord" element={<SupAdDashbord />} />         {/* mahbub.................. */}
+        <Route path="employeeList" element={<EmployeeList />} />
+        <Route path="addDepartment" element={<AddDepartment />} />
+        <Route path="addTeamLeader" element={<AddTeamLeader />} />
       </Route>
     </>
   )
