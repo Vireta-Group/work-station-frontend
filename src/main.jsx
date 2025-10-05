c79368927abbc966100125050c1792a8a0f09235
omur faruk <omurfaruk39bd@gmail.com>
2025-10-05 13:29:21 +0600

    merging omur with main

--- src/main.jsx
+++ src/main.jsx
@@ -15,34 +15,34 @@ import { ThemeProvider } from "./context/ThemeContext";
 // );
 
 // import axios from "axios";
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
 
-fetch("https://work.viretadev.com/api/work/unapproveEmployes", {
-  method: "POST",
-  headers: {
-    Authorization: `Bearer ${Cookies.get("token")}`,
-    "Content-Type": null,
-  },
-  // body: JSON.stringify({
-  //   startDate: "2-5-2024",
-  //   endDate: "4-10-2025",
-  // }),
-})
-  .then(async (res) => {
-    const data = await res.json().catch(() => null);
-    if (!res.ok) {
-      console.error("Server error:", data || res.statusText);
-      return;
-    }
-    if (data?.error) {
-      console.error(data.error);
-      return;
-    }
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error("Request failed:", error);
-  });
+// fetch("https://work.viretadev.com/api/work/unapproveEmployes", {
+//   method: "POST",
+//   headers: {
+//     Authorization: `Bearer ${Cookies.get("token")}`,
+//     "Content-Type": null,
+//   },
+//   // body: JSON.stringify({
+//   //   startDate: "2-5-2024",
+//   //   endDate: "4-10-2025",
+//   // }),
+// })
+//   .then(async (res) => {
+//     const data = await res.json().catch(() => null);
+//     if (!res.ok) {
+//       console.error("Server error:", data || res.statusText);
+//       return;
+//     }
+//     if (data?.error) {
+//       console.error(data.error);
+//       return;
+//     }
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.error("Request failed:", error);
+//   });
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider>
